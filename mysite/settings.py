1fccd060d739d7f7b241de990733dfee8c76e7dc
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-10-20 17:41:55 -0600

    hehe brr

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^b71c0t0ye!$r@98i*nat9x&)5(7@n7flc4v((x1-lu%p29wc1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
